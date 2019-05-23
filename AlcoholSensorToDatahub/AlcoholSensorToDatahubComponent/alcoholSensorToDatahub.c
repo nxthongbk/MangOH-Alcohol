0baf11bd0b548be6bb41b198cd3bf755d23cc40a
Thong Nguyen <nxthongbk@gmail.com>
2019-05-23 17:05:01 +0700

    update space

--- AlcoholSensorToDatahub/AlcoholSensorToDatahubComponent/alcoholSensorToDatahub.c
+++ AlcoholSensorToDatahub/AlcoholSensorToDatahubComponent/alcoholSensorToDatahub.c
@@ -19,21 +19,21 @@ static le_timer_Ref_t Timer;
 //--------------------------------------------------------------------------------------------------
 static void TimerExpired
 (
-	le_timer_Ref_t timer    ///< Timer reference
+    le_timer_Ref_t timer    ///< Timer reference
 )
 {
-	le_result_t res;
-	double alcohol;
+    le_result_t res;
+    double alcohol;
 
-	res= ma_alcoholSensor_Read(&alcohol);
+    res= alcoholSensor_Read(&alcohol);
 
-	if (res!= LE_OK) {
-		LE_INFO("FAILED to read alcohol sensor");
-	}
+    if (res!= LE_OK) {
+        LE_INFO("FAILED to read alcohol sensor");
+    }
 
-	LE_INFO("alcohol = %f ug/m3\n",alcohol);
+    LE_INFO("alcohol = %f \n",alcohol);
 
-	io_PushNumeric(ALCOHOL_NAME, IO_NOW, alcohol);
+    io_PushNumeric(ALCOHOL_NAME, IO_NOW, alcohol);
 
 }
 
@@ -46,30 +46,30 @@ static void TimerExpired
 //--------------------------------------------------------------------------------------------------
 static void PeriodUpdateHandler
 (
-	double timestamp,	///< time stamp
-	double value,		///< period value, seconds
-	void* contextPtr	///< not used
+    double timestamp,   ///< time stamp
+    double value,       ///< period value, seconds
+    void* contextPtr    ///< not used
 )
 //--------------------------------------------------------------------------------------------------
 {
-	LE_INFO("Received update to 'period' setting: %lf (timestamped %lf)", value, timestamp);
+    LE_INFO("Received update to 'period' setting: %lf (timestamped %lf)", value, timestamp);
 
-	uint32_t ms = (uint32_t)(value * 1000);
+    uint32_t ms = (uint32_t)(value * 1000);
 
-	if (ms == 0)
-	{
-		le_timer_Stop(Timer);
-	}
-	else
-	{
-		le_timer_SetMsInterval(Timer, ms);
+    if (ms == 0)
+    {
+        le_timer_Stop(Timer);
+    }
+    else
+    {
+        le_timer_SetMsInterval(Timer, ms);
 
-		// If the sensor is enabled and the timer is not already running, start it now.
-		if (IsEnabled && (!le_timer_IsRunning(Timer)))
-		{
-			le_timer_Start(Timer);
-		}
-    	}
+        // If the sensor is enabled and the timer is not already running, start it now.
+        if (IsEnabled && (!le_timer_IsRunning(Timer)))
+        {
+            le_timer_Start(Timer);
+        }
+        }
 }
 
 
@@ -81,30 +81,30 @@ static void PeriodUpdateHandler
 //--------------------------------------------------------------------------------------------------
 static void EnableUpdateHandler
 (
-	double timestamp,	///< time stamp
-	bool value,		///< whether input is enabled
-	void* contextPtr	///< not used
+    double timestamp,   ///< time stamp
+    bool value,     ///< whether input is enabled
+    void* contextPtr    ///< not used
 )
 //--------------------------------------------------------------------------------------------------
 {
-	LE_INFO("Received update to 'enable' setting: %s (timestamped %lf)",
- 		value == false ? "false" : "true",
-		timestamp);
+    LE_INFO("Received update to 'enable' setting: %s (timestamped %lf)",
+        value == false ? "false" : "true",
+        timestamp);
 
-	IsEnabled = value;
+    IsEnabled = value;
 
-	if (value)
-	{
-		// If the timer has a non-zero interval and is not already running, start it now.
-		if ((le_timer_GetMsInterval(Timer) != 0) && (!le_timer_IsRunning(Timer)))
-		{
-			le_timer_Start(Timer);
-		}
-	}
-	else
-	{
-		le_timer_Stop(Timer);
-	}
+    if (value)
+    {
+        // If the timer has a non-zero interval and is not already running, start it now.
+        if ((le_timer_GetMsInterval(Timer) != 0) && (!le_timer_IsRunning(Timer)))
+        {
+            le_timer_Start(Timer);
+        }
+    }
+    else
+    {
+        le_timer_Stop(Timer);
+    }
 }
 
 
@@ -115,46 +115,46 @@ static void EnableUpdateHandler
 //--------------------------------------------------------------------------------------------------
 static void UpdateStartEndHandler
 (
-	bool isStarting,    //< input is starting
-	void* contextPtr    //< Not used.
+    bool isStarting,    //< input is starting
+    void* contextPtr    //< Not used.
 )
 {
-	LE_INFO("Configuration update %s.", isStarting ? "starting" : "finished");
+    LE_INFO("Configuration update %s.", isStarting ? "starting" : "finished");
 }
 
 
 COMPONENT_INIT
 {
-	le_result_t result;
+    le_result_t result;
 
-	io_AddUpdateStartEndHandler(UpdateStartEndHandler, NULL);
+    io_AddUpdateStartEndHandler(UpdateStartEndHandler, NULL);
 
-	// This will be provided to the Data Hub.
-	result = io_CreateInput(ALCOHOL_NAME, IO_DATA_TYPE_NUMERIC, "degrees");
-	LE_ASSERT(result == LE_OK);
+    // This will be provided to the Data Hub.
+    result = io_CreateInput(ALCOHOL_NAME, IO_DATA_TYPE_NUMERIC, "times");
+    LE_ASSERT(result == LE_OK);
     
 
-	// This is my configuration setting.
-	result = io_CreateOutput(PERIOD_NAME, IO_DATA_TYPE_NUMERIC, "s");
-	LE_ASSERT(result == LE_OK);
+    // This is my configuration setting.
+    result = io_CreateOutput(PERIOD_NAME, IO_DATA_TYPE_NUMERIC, "s");
+    LE_ASSERT(result == LE_OK);
 
-	// Register for notification of updates to our configuration setting.
-	io_AddNumericPushHandler(PERIOD_NAME, PeriodUpdateHandler, NULL);
+    // Register for notification of updates to our configuration setting.
+    io_AddNumericPushHandler(PERIOD_NAME, PeriodUpdateHandler, NULL);
 
-	// This is my enable/disable control.
-	result = io_CreateOutput(ENABLE_NAME, IO_DATA_TYPE_BOOLEAN, "");
-	LE_ASSERT(result == LE_OK);
+    // This is my enable/disable control.
+    result = io_CreateOutput(ENABLE_NAME, IO_DATA_TYPE_BOOLEAN, "");
+    LE_ASSERT(result == LE_OK);
 
-	// Set the defaults: enable the sensor, set period to 1s
-	io_SetBooleanDefault(ENABLE_NAME, true);
-	io_SetNumericDefault(PERIOD_NAME, 10);
+    // Set the defaults: enable the sensor, set period to 1s
+    io_SetBooleanDefault(ENABLE_NAME, true);
+    io_SetNumericDefault(PERIOD_NAME, 10);
 
-	// Register for notification of updates to our enable/disable control.
-	io_AddBooleanPushHandler(ENABLE_NAME, EnableUpdateHandler, NULL);
+    // Register for notification of updates to our enable/disable control.
+    io_AddBooleanPushHandler(ENABLE_NAME, EnableUpdateHandler, NULL);
 
-	// Create a repeating timer that will call TimerExpired() each time it expires.
-	// Note: we'll start the timer when we receive our configuration setting.
-	Timer = le_timer_Create("airTimer");
-	le_timer_SetRepeat(Timer, 0);
-	le_timer_SetHandler(Timer, TimerExpired);
+    // Create a repeating timer that will call TimerExpired() each time it expires.
+    // Note: we'll start the timer when we receive our configuration setting.
+    Timer = le_timer_Create("alcoholTimer");
+    le_timer_SetRepeat(Timer, 0);
+    le_timer_SetHandler(Timer, TimerExpired);
 }
